21754bfe7bea62345f4ebcac4e958d6ea2780f70
Adam Edwards <adamcharlesedwards@gmail.com>
2021-12-03 00:58:55 -0500

    submit answer for AoC test code

--- src/days/1/dayone_test.go
+++ src/days/1/dayone_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"testing"
+)
 
 func TestEmptyInput(t *testing.T) {
 	actual := CountLargerMeasurements([]int{})
@@ -38,10 +43,44 @@ func TestTwoElementsWhereSecondIsGreater(t *testing.T) {
 }
 
 func TestAdventOfCodeTestData(t *testing.T) {
-	data := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+	data, err := readFile("./dayone_test_data.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
 	actual := CountLargerMeasurements(data)
 
-	if actual != 7 {
-		t.Errorf("Actual = %d; Should have been 7", actual)
+	if actual != 1139 {
+		t.Errorf("Actual = %d; Should have been 1139", actual)
 	}
+}
+
+// It would be better for such a function to return error, instead of handling
+// it on their own.
+func readFile(fname string) (nums []int, err error) {
+	b, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(b), "\n")
+	// Assign cap to avoid resize on every append.
+	nums = make([]int, 0, len(lines))
+
+	for _, l := range lines {
+		// Empty line occurs at the end of the file when we use Split.
+		if len(l) == 0 {
+			continue
+		}
+		// Atoi better suits the job when we know exactly what we're dealing
+		// with. Scanf is the more general option.
+		n, err := strconv.Atoi(l)
+		if err != nil {
+			return nil, err
+		}
+		nums = append(nums, n)
+	}
+
+	return nums, nil
 }
